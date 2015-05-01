aec1950c083f0c97159556454a43e1120fc2b27e
mark <markesargent@gmail.com>
2015-04-30 17:48:43 -0700

    added files

---
+++ FlickrGrabber/app/src/main/java/com/detroitteatime/flickrgrabber/Constants.java
@@ -0,0 +1,12 @@
+package com.detroitteatime.flickrgrabber;
+
+/**
+ * Created by mark on 4/29/15.
+ */
+public class Constants {
+    public static final int NUM_PHOTOS = 10;
+    public static final String TAG = "my code";
+
+
+
+}
