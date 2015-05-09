b98d261b59123cb97ff214730d557b6a8e8f7a0e
mark <markesargent@gmail.com>
2015-05-09 10:46:08 -0700

    Database added

--- MyFlickr/app/src/main/java/com/detroitteatime/myflickr/Contract.java
+++ MyFlickr/app/src/main/java/com/detroitteatime/myflickr/Contract.java
@@ -1,20 +1,18 @@
 package com.detroitteatime.myflickr;
 
 import android.provider.BaseColumns;
-
 /**
  * Created by mark on 5/7/15.
  */
 //String id, owner, secret, server, farm, title;
 //        Boolean isPublic, isFriend, isFamily;
 
-
 public class Contract{
+    public static final String DATABASE_NAME = "myflickr.db";
 
     public static final class PhotoEntry implements BaseColumns{
 
-        public static final String TABLE_NAME = "photo";
-
+        public static final String TABLE_NAME = "photo_entry";
         public static final String OWNER = "owner";
         public static final String SECRET = "secret";
         public static final String SERVER = "server";
@@ -24,9 +22,6 @@ public class Contract{
         public static final String FRIEND = "friend";
         public static final String FAMILY = "family";
 
-
     }
-
-
 
 }
